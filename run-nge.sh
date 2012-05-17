ef196992a16ce1925e04cd72053ed8b8d57c9337
Damien Raude-Morvan <drazzib@debian.org>
2012-05-17 02:53:48 +0200

    Small fixes to script (eg. wrong filename).

--- run-nge.sh
+++ run-nge.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-rm horaires_parkings-nm.csv
+rm horaires_parkings-nge.csv
 
 java -Djava.net.preferIPv4Stack=true \
   -jar webharvest_all_2.jar \
-  config=nantesmetropole-parkings-horaires.xml
+  config=nge-parkings-horaires.xml
 
-cat horaires_parkings-nm.csv
+cat horaires_parkings-nge.csv
